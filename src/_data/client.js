04181c0c1c3db8117f80f05e875202b7b55567a4
Josh <jmurd12@proton.me>
2024-08-25 16:46:14 -0400

    initial commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
     "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "connorEmail": "tcburch@burchindustrialservices.net",
+    "connorPhoneForTel": "770-231-4055",
+    "connorPhoneFormatted": "(770) 231-4055",
+    "holtenPhoneForTel": "770-312-0046",
+    "holtenPhoneFormatted": "(770) 312-0046",
+    "andyPhoneForTel": "770-881-1485",
+    "andyPhoneFormatted": "(770) 882-1485",
+    "tommyPhoneForTel": "404-274-0102",
+    "tommyPhoneFormatted": "(404) 274-0102",
     "address": {
         "lineOne": "First Address Line",
         "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "city": "Loganville",
+        "state": "Ga",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.burchindustrialservices.net"
 };
